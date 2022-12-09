8c4241ae1ade9cb45c29ee61a15d7bed8b15eadf
Omnia samy <omnia@Ibtikars-MacBook-Pro.local>
2022-12-09 11:21:29 +0200

    Work Done: 1- bind currency cell data

--- VOM-Task/VOM-Task/Scenes/Home/Cell/CurrencyTableViewCell.swift
+++ VOM-Task/VOM-Task/Scenes/Home/Cell/CurrencyTableViewCell.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class CurrencyTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var currencyImageView: UIImageView!
+    @IBOutlet private weak var currencyNameLabel: UILabel!
+    @IBOutlet private weak var currencyValueLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
     func bind(currency: CurrencyModel) {
-        
+//    currencyImageView
+        //placeholder_Icon
+        currencyNameLabel.text = currency.key
+        currencyValueLabel.text = String(currency.value ?? 0)
     }
-    
 }
