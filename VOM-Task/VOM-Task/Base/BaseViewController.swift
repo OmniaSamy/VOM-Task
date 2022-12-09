15895d8830731ab1fb55a9573114bb1391af4891
Omnia samy <omnia@Ibtikars-MacBook-Pro.local>
2022-12-09 10:10:05 +0200

    Work Done: 1- add loader and baseview

---
+++ VOM-Task/VOM-Task/Base/BaseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BaseViewController.swift
+//  VOM-Task
+//
+//  Created by Omnia on 09/12/2022.
+//
+
+import Foundation
---
+++ VOM-Task/VOM-Task/Custom/Loader/Loader.swift
@@ -0,0 +1,8 @@
+//
+//  Loader.swift
+//  VOM-Task
+//
+//  Created by Omnia on 09/12/2022.
+//
+
+import Foundation
