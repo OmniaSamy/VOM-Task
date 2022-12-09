5bc3f61c3dd36cf5e812846c0a7ed8e97a5dc167
Omnia samy <omnia@Ibtikars-MacBook-Pro.local>
2022-12-09 11:08:52 +0200

    Work Done: 1- make currency cell module

---
+++ VOM-Task/VOM-Task/Extension/NSObject+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  NSObject+Extension.swift
+//  VOM-Task
+//
+//  Created by Omnia on 09/12/2022.
+//
+
+import Foundation
---
+++ VOM-Task/VOM-Task/Scenes/Home/Cell/CurrencyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyTableViewCell.swift
+//  VOM-Task
+//
+//  Created by Omnia on 09/12/2022.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
